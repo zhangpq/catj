b222f1eff7bd4a9df65b52670002cefd03c141d6
hqa11 <hqa4653297>
2018-09-30 19:41:45 +0800

    no~

--- catj-zipkin/src/main/java/com/catj/CatjZipkinApplication.java
+++ catj-zipkin/src/main/java/com/catj/CatjZipkinApplication.java
@@ -2,10 +2,12 @@ package com.catj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @EnableZipkinServer
 @SpringBootApplication
+@EnableEurekaClient
 public class CatjZipkinApplication {
 
 	public static void main(String[] args) {
