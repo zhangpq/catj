46c55b5081e3784b5966008b741fa99988a44451
mc.han <mc.han.@ly.com>
2018-09-30 13:54:54 +0800

    first

--- catj-user-service/src/main/java/com/catj/CatjUserServiceApplication.java
+++ catj-user-service/src/main/java/com/catj/CatjUserServiceApplication.java
@@ -2,23 +2,14 @@ package com.catj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-
-import javax.persistence.EntityManagerFactory;
 
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker//打开Hystrix断路器
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableJpaRepositories(basePackages = "com.catj.dao")
+@SpringBootApplication
 public class CatjUserServiceApplication {
 
 	public static void main(String[] args) {
